f5df52641222cfc0281a72ab561db743517c971a
Akshat76845 <88035400+Akshat76845@users.noreply.github.com>
2024-06-18 02:33:34 +0530

    Update loading.jsx

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,16 @@
+"use client";
+import Image from "next/image";
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <img className="h-52" src="/spinner.svg" alt="loading..." />
+      <Image
+        className="h-52"
+        src="/spinner.svg"
+        alt="loading..."
+        width={800}
+        height={100}
+        priority
+      />
     </div>
   );
 }
