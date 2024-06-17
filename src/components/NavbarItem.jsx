aea6f1eb49680fe4593ad6a766db289b7ca78b57
Akshat76845 <88035400+Akshat76845@users.noreply.github.com>
2024-06-18 03:36:32 +0530

    Update NavbarItem.jsx

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,26 +1,14 @@
-"use client"
+// components/NavbarItem.jsx
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
-export default function NavbarItem({ title, param, extraClasses = "" }) {
-  const searchParams = useSearchParams();
-  const genre = searchParams.get("genre");
-
+const NavbarItem = ({ title, param, extraClasses = "" }) => {
   return (
     <div className={extraClasses}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Link
-          href={`/?genre=${param}`}
-          className={`hover:text-amber-600 font-semibold ${
-            genre === param
-              ? "decoration-4 decoration-amber-500 rounded-lg"
-              : "underline underline-offset-8 mr-16"
-          }`}
-        >
-          {title}
-        </Link>
-      </Suspense>
+      <Link href={`/?genre=${param}`}>
+        <div className="hover:text-amber-600 font-semibold">{title}</div>
+      </Link>
     </div>
   );
-}
+};
+
+export default NavbarItem;
